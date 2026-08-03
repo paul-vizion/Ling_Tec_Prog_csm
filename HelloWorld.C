4a7c617f8b3c0c5e61eeefbeaf3197a3f4e8f7df
paul-vizion <paulonlp008@gmail.com>
2026-08-03 09:14:51 -0300

    HelloWorld.C

--- HelloWorld.C
+++ HelloWorld.C
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 int main(){
-     printf("𝐅𝐨𝐫𝐭𝐞 𝐚𝐛𝐫𝐚𝐜̧𝐨 𝐌𝐚𝐥𝐚𝐧𝐝𝐫𝐚𝐠𝐞\n ");
-     return 69;
-}
+        printf("Hello World!\n");
+        system("pause");
+        return 0;
+		}
